3251c7412ba56e9e64e4b2c6615f30efef53fc92
abdellah <Abdellah.chehri14@gmail.com>
2024-09-17 00:55:41 +0200

    make the homepage

--- apps/web/app/(dashboard)/dashboard/page.tsx
+++ apps/web/app/(dashboard)/dashboard/page.tsx
@@ -1,15 +1,50 @@
 import Header from "@/components/layout/header";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { OnboardingDialog } from "@/components/modals/onboading-modal";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 import type { FC } from "react";
 
+// here fetch to see if the user has been onboarded or not
 const page: FC = () => {
+  // just send the data to the endpoint and finish that modal
   return (
     <>
-      <div className="w-full h-[700px] flex items-center justify-center">
-        <h1 className="text-2xl font-bold ">this is the dashboard</h1>
+      <div className="bg-white w-full ">
+        <MaxWidthWrapper>
+          <div className="w-full h-[400px] grid grid-cols-3 ">
+            <div className="flex flex-col justify-center p-4 gap-y-4 col-span-2">
+              <h2 className="text-4xl font-bold text-start">
+                Want to contribute to projects <br /> but lack the necessary
+                skills?
+              </h2>
+              <Link
+                href="/auth"
+                className="px-4 py-4 rounded-md font-bold text-lg bg-primary text-white border-2 border-black  w-fit transition-all shadow-[4px_4px_0px_black] hover:shadow-none hover:translate-x-[4px] hover:translate-y-[4px]"
+              >
+                Explore different project
+              </Link>
+            </div>
+
+            <div className="w-full h-[400px] relative col-span-1">
+              <Image
+                alt="ballon images"
+                src={"/gifts.svg"}
+                fill
+                loading="eager"
+                className="object-contain"
+              />
+            </div>
+          </div>
+        </MaxWidthWrapper>
       </div>
 
-      {/* <OnboardingDialog /> */}
+      <MaxWidthWrapper>
+        <div className="w-full min-h-screen h-fit flex flex-col items-center justify-start"></div>
+      </MaxWidthWrapper>
+
+      <OnboardingDialog />
     </>
   );
 };
---
+++ apps/web/components/user-nav.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { LogOut, Settings, User } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="cursor-pointer">
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
