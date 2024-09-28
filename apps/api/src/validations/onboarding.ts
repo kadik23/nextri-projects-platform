1426a1bd732e09de77d8c9f8a77b2aa9fdcbabfc
abdellah <Abdellah.chehri14@gmail.com>
2024-09-28 13:27:02 +0200

    fix: make the tech stack as a string not string[]

--- apps/api/src/validations/onboarding.ts
+++ apps/api/src/validations/onboarding.ts
@@ -1,28 +1,28 @@
 import { z } from "zod";
 
 export const WorkPaceEnum = z.enum([
-	"short_term",
-	"medium_term",
-	"long_term",
-	"specific_task",
+  "short_term",
+  "medium_term",
+  "long_term",
+  "specific_task",
 ]);
 
 export const ProjectCategoryPreferenceEnum = z.enum([
-	"freelance",
-	"open_source",
-	"company",
+  "freelance",
+  "open_source",
+  "company",
 ]);
 
 export const OpenSourcePathEnum = z.enum(["rebuild_projects", "solve_issues"]);
 
 export const onboardingSchema = z.object({
-	role: z.string(),
-	skills: z.string().array(),
-	project_types: z.array(ProjectCategoryPreferenceEnum),
-	project_foucus: z.string().array(),
-	skill_level: z.string(),
-	work_pace: WorkPaceEnum,
-	work_types: z.array(OpenSourcePathEnum),
+  role: z.string(),
+  skills: z.string(),
+  project_types: z.array(ProjectCategoryPreferenceEnum),
+  project_foucus: z.string().array(),
+  skill_level: z.string(),
+  work_pace: WorkPaceEnum,
+  work_types: z.array(OpenSourcePathEnum),
 });
 
 export type TOnboardingSchema = z.infer<typeof onboardingSchema>;
